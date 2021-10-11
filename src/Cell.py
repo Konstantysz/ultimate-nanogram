43bacc10cbf9ba88712a96ba96cc50e2c9d42616
konstantysz <kostekszumigaj@gmail.com>
2021-10-11 22:00:18 +0200

    Correct Cell class

--- src/Cell.py
+++ src/Cell.py
@@ -1,18 +1,12 @@
 class Cell:
  
     # default constructor
-    def __init__(self):
+    def __init__(self, value):
         self._clicked = False
-        self._value = False
-
-    def GetValue(self):
-        return self._value
+        self._value = value
 
     def GetClicked(self):
         return self._clicked
-
-    def SetValue(self, value):
-        self._value = value
 
     def SetClick(self, clicked):
         self._clicked = clicked
