47140ab2b238d794080effbf09c520a5f76a9535
konstantysz <kostekszumigaj@gmail.com>
2021-10-11 22:36:31 +0200

    Implement new consts

--- src/GameController.py
+++ src/GameController.py
@@ -1,7 +1,10 @@
 from src.BoardController import BoardController
+from src.GameConsts import LIVESNUMBER, InputMode
 
 class GameController:
 
     # default constructor
     def __init__(self, boardSize):
+        self._lives = LIVESNUMBER
+        self._input_mode = InputMode.IMAGECELL
         self._gameController = BoardController(boardSize)
