869610b7ab78f65761c0cf4deb676b9e8b718d87
konstantysz <kostekszumigaj@gmail.com>
2021-10-11 21:53:16 +0200

    Add getters and setters for Cell

--- src/Cell.py
+++ src/Cell.py
@@ -4,3 +4,15 @@ class Cell:
     def __init__(self):
         self._clicked = False
         self._value = False
+
+    def GetValue(self):
+        return self._value
+
+    def GetClicked(self):
+        return self._clicked
+
+    def SetValue(self, value):
+        self._value = value
+
+    def SetClick(self, clicked):
+        self._clicked = clicked
