4545015523e43202efe04bcbe88ff4f376631bf2
konstantysz <kostekszumigaj@gmail.com>
2021-10-11 22:39:35 +0200

    Add methods for Board class

--- src/BoardController.py
+++ src/BoardController.py
@@ -5,3 +5,9 @@ class BoardController:
     # default constructor
     def __init__(self, boardSize):
         print("Board {}x{} created!".format(boardSize.value, boardSize.value))
+
+    def import_board_image(self):
+        return
+
+    def calculate_info_numbers(self):
+        return
